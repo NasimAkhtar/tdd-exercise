77978e16365bb75698a2e1f02738a993685d781a
nasimakhtar <nasim@backbase.com>
2021-01-06 01:24:14 +0800

    Test for add_forEmptyString_returnsZero

--- src/test/java/com/nasim/exercise/tdd/SimpleStringCalculatorTest.java
+++ src/test/java/com/nasim/exercise/tdd/SimpleStringCalculatorTest.java
@@ -2,10 +2,15 @@ package com.nasim.exercise.tdd;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class SimpleStringCalculatorTest {
     @Test
     public void add_forEmptyString_returnsZero() {
         SimpleStringCalculator simpleStringCalculator = new SimpleStringCalculator();
-        simpleStringCalculator.add("");
+
+        int result = simpleStringCalculator.add("");
+
+        assertEquals(0, result);
     }
 }
