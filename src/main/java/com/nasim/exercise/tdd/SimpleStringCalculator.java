600798fa40a585c94c4f8429a065fd2ea3b3435e
nasimakhtar <nasim@backbase.com>
2021-01-06 11:24:24 +0800

    Refactored to pass both tests

--- src/main/java/com/nasim/exercise/tdd/SimpleStringCalculator.java
+++ src/main/java/com/nasim/exercise/tdd/SimpleStringCalculator.java
@@ -2,6 +2,7 @@ package com.nasim.exercise.tdd;
 
 public class SimpleStringCalculator {
     public int add(String numbers) {
+        if (numbers.isEmpty()) return 0;
         return Integer.parseInt(numbers);
     }
 }
