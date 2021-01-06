8dc9249673391d399cb4f55ea6bbdb2e0bb7be43
nasimakhtar <nasim@backbase.com>
2021-01-07 01:05:00 +0800

    Impl for add_withNegativeNumbers_alwaysThrows

--- src/main/java/com/nasim/exercise/tdd/SimpleStringCalculator.java
+++ src/main/java/com/nasim/exercise/tdd/SimpleStringCalculator.java
@@ -3,6 +3,7 @@ package com.nasim.exercise.tdd;
 public class SimpleStringCalculator {
     public int add(String numbers) {
         if (numbers.isEmpty()) return 0;
+        if (numbers.contains("-")) throw new NumberFormatException("negatives not allowed" + numbers);
         if (numbers.length() == 1) return Integer.parseInt(numbers);
         return parse(numbers);
     }
